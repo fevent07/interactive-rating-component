005b06ca1a128c10019336d75e9c4bb3df1e6f8f
feven <46973832+fevent07@users.noreply.github.com>
2023-08-28 16:42:14 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        White: 'hsl(0, 0%, 100%)',
+        LightGrey: 'hsl(217, 12%, 63%)',
+        MediumGrey: 'hsl(216, 12%, 54%)',
+        DarkBlue: 'hsl(213, 19%, 18%)',
+        VeryDarkBlue: 'hsl(216, 12%, 8%)',
+        Orange: 'hsl(25, 97%, 53%)',
+        DarkGrey: 'hsl(213, 20%, 22%)'
+      },
+      fontFamily: {
+
+        'sans': ['Overpass', 'sans-serif']
+
+      },
+    },
   },
   plugins: [],
 }
-
